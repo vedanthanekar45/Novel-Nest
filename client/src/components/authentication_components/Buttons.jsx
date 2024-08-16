a1e82b517ac57994a34653d47bbe76c2e21f84fe
Youssef HIMMI <yhimmi1@gmail.com>
2024-08-16 19:31:52 +0200

    fixing space under navbar element styling issue

--- client/src/components/authentication_components/Buttons.jsx
+++ client/src/components/authentication_components/Buttons.jsx
@@ -1,38 +1,31 @@
-function Buttons () {
-
+function Buttons() {
     const storedUser = localStorage.getItem("chat-user");
 
     if (!storedUser) {
         return (
-            <div>
+            <div className="nav-buttons">
                 <a href="/login">
-                    <button className="login-butt text-white cursor-pointer outline-none 
-                    border-none text-base text-center transition-all 
-                    duration-300 ease-linear relative bg-[#0d9221] 
-                    rounded-[10px] hover:bg-[#2db40c]">
+                    <button className="login-butt">
                         Login
                     </button>
                 </a>
                 <a href="/signup">
-                    <button className="register-butt text-white cursor-pointer outline-none 
-                    border-none text-base text-center transition-all 
-                    duration-300 ease-linear relative bg-[#0d9221] 
-                    rounded-[10px] hover:bg-[#2db40c]">
+                    <button className="register-butt">
                         Register
                     </button>
                 </a>
             </div>
-        )
+        );
     } else {
         const user = JSON.parse(storedUser);
         return (
-            <>
-                <a href="#"><div className="avatar bottom-4">
-                    <img className="webpage-butt size-16 rounded-full" src={user.profilePic} />
-                </div></a>
-            </>
-        )
+            <a href="#">
+                <div className="avatar bottom-4">
+                    <img className="webpage-butt size-16 rounded-full" src={user.profilePic} alt="User Avatar" />
+                </div>
+            </a>
+        );
     }
 }
 
-export default Buttons
+export default Buttons;
